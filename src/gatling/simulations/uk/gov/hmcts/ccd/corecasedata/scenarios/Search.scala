6e492b694205974dd237f711ffa15681a1a960dd
jmcadam <mcadam88@gmail.com>
2019-07-22 14:24:20 +0100

    Replace jurisdiction and case type params

--- src/gatling/simulations/uk/gov/hmcts/ccd/corecasedata/scenarios/Search.scala
+++ src/gatling/simulations/uk/gov/hmcts/ccd/corecasedata/scenarios/Search.scala
@@ -12,22 +12,22 @@ object Search {
 	val CCDCreateCaseFeeder = csv("CCD_CreateCase_TestData.csv").circular
 
   val searchRequest = exec(http("CDM_160_005_SearchRequest")
-		.options("/data/caseworkers/:uid/jurisdictions/AUTOTEST1/case-types/AAT_PRIVATE/cases/pagination_metadata"))
+		.options("/data/caseworkers/:uid/jurisdictions/${Jurisdiction}/case-types/${CaseType}/cases/pagination_metadata"))
 
 		.exec(http("CDM_160_005_SearchRequest")
-			.get("/data/caseworkers/:uid/jurisdictions/AUTOTEST1/case-types/AAT_PRIVATE/cases/pagination_metadata")
+			.get("/data/caseworkers/:uid/jurisdictions/${Jurisdiction}/case-types/${CaseType}/cases/pagination_metadata")
 			.headers(CommonHeader))
 
 		.pause(MinThinkTime seconds, MaxThinkTime seconds)
 		
 	val searchResult = exec(http("CDM_170_005_SearchResult")
-		.options("/aggregated/caseworkers/:uid/jurisdictions/AUTOTEST1/case-types/AAT_PRIVATE/cases?view=WORKBASKET&page=1"))
+		.options("/aggregated/caseworkers/:uid/jurisdictions/${Jurisdiction}/case-types/${CaseType}/cases?view=WORKBASKET&page=1"))
 
 		.exec(http("CDM_170_010_SearchResult")
 			.options("/data/caseworkers/:uid/jurisdictions/${Jurisdiction}/case-types/${CaseType}/cases/pagination_metadata?state=TODO"))
 
 		.exec(http("CDM_170_015_SearchResult")
-			.get("/aggregated/caseworkers/:uid/jurisdictions/AUTOTEST1/case-types/AAT_PRIVATE/cases?view=WORKBASKET&page=1")
+			.get("/aggregated/caseworkers/:uid/jurisdictions/${Jurisdiction}/case-types/${CaseType}/cases?view=WORKBASKET&page=1")
 			.headers(CommonHeader)
 			.check(jsonPath("$.results[*].case_id").saveAs("SearchParam_Case_Id")))
 
