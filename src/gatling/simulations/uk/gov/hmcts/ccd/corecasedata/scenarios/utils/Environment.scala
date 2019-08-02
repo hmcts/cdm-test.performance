b8c348ba0811b8197416181b9a0a29f2c1416228
jmcadam <mcadam88@gmail.com>
2019-08-02 15:46:47 +0100

    Update Environment.scala

--- src/gatling/simulations/uk/gov/hmcts/ccd/corecasedata/scenarios/utils/Environment.scala
+++ src/gatling/simulations/uk/gov/hmcts/ccd/corecasedata/scenarios/utils/Environment.scala
@@ -8,13 +8,13 @@ import io.gatling.jdbc.Predef._
 
 object Environment {
 
-  val idamURL = "https://idam-web-public.perftest.platform.hmcts.net"
+  /*val idamURL = "https://idam-web-public.perftest.platform.hmcts.net"
   val ccdEnvurl = "https://ccd-case-management-web-perftest.service.core-compute-perftest.internal"
-  val baseURL = "https://ccd-api-gateway-web-perftest.service.core-compute-perftest.internal"
+  val baseURL = "https://ccd-api-gateway-web-perftest.service.core-compute-perftest.internal"*/
 
-  /*val idamURL = "https://idam-web-public.aat.platform.hmcts.net"
+  val idamURL = "https://idam-web-public.aat.platform.hmcts.net"
   val ccdEnvurl = "https://ccd-case-management-web-aat.service.core-compute-aat.internal"
-  val baseURL = "https://ccd-api-gateway-web-aat.service.core-compute-aat.internal"*/
+  val baseURL = "https://ccd-api-gateway-web-aat.service.core-compute-aat.internal"
 
  // val baseURL = "https://gateway.ccd.demo.platform.hmcts.net"
   //val idamURL = "https://idam.preprod.ccidam.reform.hmcts.net"
