a2fb6a93ae61f33388908aea0769b3d01f2ffb4d
jmcadam <mcadam88@gmail.com>
2019-08-08 10:19:12 +0100

    Update CCDUIPTSimulation.scala

--- src/gatling/simulations/uk/gov/hmcts/ccd/corecasedata/simulations/CCDUIPTSimulation.scala
+++ src/gatling/simulations/uk/gov/hmcts/ccd/corecasedata/simulations/CCDUIPTSimulation.scala
@@ -44,7 +44,7 @@ class CCDUIPTSimulation extends Simulation  {
 
   //setUp(CCDUIScenario.inject(atOnceUsers(1))).protocols(httpProtocol)
   setUp(CCDUIScenario
-    .inject(rampUsers(10) during (20 minutes))
+    .inject(rampUsers(1000) during (20 minutes))
     .protocols(httpProtocol))
     .maxDuration(80 minutes)
 }
