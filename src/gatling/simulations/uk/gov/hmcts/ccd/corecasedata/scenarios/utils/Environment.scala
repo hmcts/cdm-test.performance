7897784bc9be81b3d274a39703cd13284918c577
jmcadam <mcadam88@gmail.com>
2019-08-19 18:39:29 +0100

    Update Environment.scala

--- src/gatling/simulations/uk/gov/hmcts/ccd/corecasedata/scenarios/utils/Environment.scala
+++ src/gatling/simulations/uk/gov/hmcts/ccd/corecasedata/scenarios/utils/Environment.scala
@@ -19,8 +19,8 @@ object Environment {
  // val baseURL = "https://gateway.ccd.demo.platform.hmcts.net"
   //val idamURL = "https://idam.preprod.ccidam.reform.hmcts.net"
   //val ccdEnvurl = "https://www.ccd.demo.platform.hmcts.net" 
-  val minThinkTime = 1
-  val maxThinkTime = 1
+  val minThinkTime = 5
+  val maxThinkTime = 12
   val constantthinkTime = 7
   val minWaitForNextIteration = 300
   val maxWaitForNextIteration = 600
