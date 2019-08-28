cedbc409efd6ab900427ed833035d4c27b2c5b2b
jmcadam <mcadam88@gmail.com>
2019-08-28 13:12:20 +0100

    fixes for pipeline

--- src/gatling/simulations/uk/gov/hmcts/ccd/corecasedata/simulations/CCDUIPTSimulation.scala
+++ src/gatling/simulations/uk/gov/hmcts/ccd/corecasedata/simulations/CCDUIPTSimulation.scala
@@ -2,7 +2,6 @@ package uk.gov.hmcts.ccd.corecasedata.simulations
 
 import io.gatling.core.Predef._
 import io.gatling.core.scenario.Simulation
-import io.gatling.http.Predef.Proxy
 import uk.gov.hmcts.ccd.corecasedata.scenarios._
 import uk.gov.hmcts.ccd.corecasedata.scenarios.utils._
 
