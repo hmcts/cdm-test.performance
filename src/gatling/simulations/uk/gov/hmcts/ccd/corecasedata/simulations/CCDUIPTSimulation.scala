62b93ac43cccb7819f943b34a10cc8d66bd2d223
jmcadam <mcadam88@gmail.com>
2019-10-18 10:41:53 +0100

    Update CCDUIPTSimulation.scala

--- src/gatling/simulations/uk/gov/hmcts/ccd/corecasedata/simulations/CCDUIPTSimulation.scala
+++ src/gatling/simulations/uk/gov/hmcts/ccd/corecasedata/simulations/CCDUIPTSimulation.scala
@@ -38,7 +38,7 @@ class CCDUIPTSimulation extends Simulation  {
     )
   }
 
-  val CCDProbateScenario = scenario("CCDPB").repeat(5) {
+  val CCDProbateScenario = scenario("CCDPB").repeat(7) {
     exec(
       Browse.Homepage,
       ExecuteLogin.submitLogin,
@@ -52,7 +52,7 @@ class CCDUIPTSimulation extends Simulation  {
     )
   }
 
-  val CCDSSCSScenario = scenario("CCDSSCS").repeat(6)
+  val CCDSSCSScenario = scenario("CCDSSCS").repeat(7)
   {
     exec(
       Browse.Homepage,
@@ -67,7 +67,7 @@ class CCDUIPTSimulation extends Simulation  {
     )
   }
 
-  val CCDCMCScenario = scenario("CCDCMC").repeat(6)
+  val CCDCMCScenario = scenario("CCDCMC").repeat(7)
   {
     exec(
       Browse.Homepage,
@@ -83,7 +83,7 @@ class CCDUIPTSimulation extends Simulation  {
     )
   }
 
-  val CCDDivScenario = scenario("CCDDIV").repeat(6)
+  val CCDDivScenario = scenario("CCDDIV").repeat(7)
   {
     exec(
       Browse.Homepage,
@@ -96,7 +96,7 @@ class CCDUIPTSimulation extends Simulation  {
     )
   }
 
-  val CCDEthosScenario = scenario("CCDEthos").repeat(12)
+  val CCDEthosScenario = scenario("CCDEthos").repeat(11)
   {
     exec(
       Browse.Homepage,
