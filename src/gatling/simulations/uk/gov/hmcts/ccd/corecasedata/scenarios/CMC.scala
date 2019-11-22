aebb40a1434e6a28b1d73555788587703a4ff17d
jmcadam <mcadam88@gmail.com>
2019-11-22 11:54:48 +0000

    Update CMC.scala

--- src/gatling/simulations/uk/gov/hmcts/ccd/corecasedata/scenarios/CMC.scala
+++ src/gatling/simulations/uk/gov/hmcts/ccd/corecasedata/scenarios/CMC.scala
@@ -3,6 +3,7 @@ package uk.gov.hmcts.ccd.corecasedata.scenarios
 import io.gatling.core.Predef._
 import io.gatling.http.Predef._
 import uk.gov.hmcts.ccd.corecasedata.scenarios.utils.Environment
+
 import scala.concurrent.duration._
 
 object CMC {
@@ -44,92 +45,201 @@ object CMC {
     "Sec-Fetch-Mode" -> "cors",
     "experimental" -> "true")
 
+  val headers_9 = Map(
+    "Accept" -> "application/vnd.uk.gov.hmcts.ccd-data-store-api.create-event.v2+json;charset=UTF-8",
+    "Content-Type" -> "application/json",
+    "Origin" -> "https://ccd-case-management-web-perftest.service.core-compute-perftest.internal",
+    "Sec-Fetch-Mode" -> "cors",
+    "Sec-Fetch-Site" -> "cross-site",
+    "experimental" -> "true")
+
   val headers_11 = Map(
     "Accept" -> "application/vnd.uk.gov.hmcts.ccd-data-store-api.ui-start-event-trigger.v2+json;charset=UTF-8",
     "Sec-Fetch-Mode" -> "cors",
     "experimental" -> "true")
 
+  val CMCLogin = group("CMC_Login") {
 
-  val CMCCreateCase = group("CMC_Create") {
+    exec(http("CDM_020_005_Login")
+      .post(IdamURL + "/login?response_type=code&client_id=ccd_gateway&redirect_uri=" + CCDEnvurl + "/oauth2redirect")
+      .disableFollowRedirect
+      .headers(idam_header)
+      .formParam("username", "${CMCUserName}")
+      .formParam("password", "${CMCUserPassword}")
+      .formParam("save", "Sign in")
+      .formParam("selfRegistrationEnabled", "false")
+      .formParam("_csrf", "${csrf}")
+      .check(headerRegex("Location", "(?<=code=)(.*)&scope").saveAs("authCode"))
+      .check(status.in(200, 302))
 
-    exec(http("CMC_030_005_CreateCasePage")
+      .resources(http("CDM_020_010_Login")
+        .get(CCDEnvurl + "/config")
+        .headers(CommonHeader)))
+
+      .exec(http("CDM_020_015_Login")
+        .options(BaseURL + "/oauth2?code=${authCode}&redirect_uri=" + CCDEnvurl + "/oauth2redirect")
+        .headers(CommonHeader))
+      .exec(http("CDM_020_020_Login")
+        .get(BaseURL + "/oauth2?code=${authCode}&redirect_uri=" + CCDEnvurl + "/oauth2redirect")
+        .headers(CommonHeader))
+      .exec(http("CDM_020_025_Login")
+        .get(CCDEnvurl + "/config")
+        .headers(CommonHeader))
+
+      .exec(http("CDM_020_030_Login")
+        .options(BaseURL + "/data/caseworkers/:uid/profile"))
+
+      .exec(http("CDM_020_035_Login")
+        .get(BaseURL + "/data/caseworkers/:uid/profile")
+        .headers(CommonHeader))
+
+      .exec(http("CDM_020_040_Login")
+        .options(BaseURL + "/aggregated/caseworkers/:uid/jurisdictions/CMC/case-types?access=read")
+        .resources(http("CDM_020_045_Login")
+          .get(BaseURL + "/aggregated/caseworkers/:uid/jurisdictions/CMC/case-types?access=read")
+          .headers(CommonHeader)))
+
+      .exec(http("CDM_020_050_Login")
+        .options(BaseURL + "/aggregated/caseworkers/:uid/jurisdictions/CMC/case-types/MoneyClaimCase/work-basket-inputs"))
+
+      .exec(http("CDM_020_055_Login")
+        .options(BaseURL + "/aggregated/caseworkers/:uid/jurisdictions/CMC/case-types/MoneyClaimCase/cases?view=WORKBASKET&state=TODO&page=1"))
+
+      .exec(http("CDM_020_060_Login")
+        .options(BaseURL + "/data/caseworkers/:uid/jurisdictions/CMC/case-types/MoneyClaimCase/cases/pagination_metadata?state=TODO"))
+
+      .exec(http("CDM_020_065_Login")
+        .get(BaseURL + "/aggregated/caseworkers/:uid/jurisdictions/CMC/case-types/MoneyClaimCase/work-basket-inputs")
+        .headers(CommonHeader))
+
+      .exec(http("CDM_020_070_Login")
+        .get(BaseURL + "/aggregated/caseworkers/:uid/jurisdictions/CMC/case-types/MoneyClaimCase/cases?view=WORKBASKET&state=TODO&page=1")
+        .headers(CommonHeader))
+
+      .exec(http("CDM_020_075_Login")
+        .get(BaseURL + "/data/caseworkers/:uid/jurisdictions/CMC/case-types/MoneyClaimCase/cases/pagination_metadata?state=TODO")
+        .headers(CommonHeader))
+  }
+
+    .pause(MinThinkTime seconds, MaxThinkTime seconds)
+
+  val CMCCreateCase = exec(http("CMC_030_005_CreateCasePage")
       .get(BaseURL + "/aggregated/caseworkers/:uid/jurisdictions?access=create")
       .headers(CommonHeader))
 
       .pause(MinThinkTime seconds, MaxThinkTime seconds)
 
     .exec(http("CMC_030_010_CreateCaseDetails")
-      .get(BaseURL + "/data/internal/case-types/MoneyClaimCase/event-triggers/SubmitPrePayment?ignore-warning=false")
+      .get(BaseURL + "/data/internal/case-types/MoneyClaimCase/event-triggers/CreateClaim?ignore-warning=false")
       .headers(headers_1)
       .check(jsonPath("$.event_token").saveAs("New_Case_event_token")))
 
       .pause(MinThinkTime seconds, MaxThinkTime seconds)
 
-    .exec(http("CMC_030_015_CreateCase")
-      //.post(BaseURL + "/data/caseworkers/:uid/jurisdictions/CMC/case-types/MoneyClaimCase/cases?ignore-warning=false")
-      .post(BaseURL + "/data/case-types/MoneyClaimCase/validate?pageId=SubmitPrePayment1")
-      .headers(headers_3)
-      .body(StringBody("{\n  \"data\": {\n    \"externalId\": \"cmc-id\"\n  },\n  \"event\": {\n    \"id\": \"SubmitPrePayment\",\n    \"summary\": \"cmc-create-case\",\n    \"description\": \"\"\n  },\n  \"event_token\": \"${New_Case_event_token}\",\n  \"ignore_warning\": false,\n  \"draft_id\": null\n}")))
-
-    .exec(http("CMC_030_020_CreateCase")
-      .post("/data/caseworkers/:uid/jurisdictions/CMC/case-types/MoneyClaimCase/cases?ignore-warning=false")
+    .exec(http("CMC_030_015_CreateCaseSubmit")
+      .post(BaseURL + "/data/caseworkers/:uid/jurisdictions/CMC/case-types/MoneyClaimCase/cases?ignore-warning=false")
+      //.post(BaseURL + "/data/case-types/MoneyClaimCase/validate?pageId=SubmitPrePayment1")
       .headers(CommonHeader)
-      .body(StringBody("{\n  \"data\": {\n    \"externalId\": null\n  },\n  \"event\": {\n    \"id\": \"SubmitPrePayment\",\n    \"summary\": \"\",\n    \"description\": \"\"\n  },\n  \"event_token\": \"${New_Case_event_token}\",\n  \"ignore_warning\": false,\n  \"draft_id\": null\n}"))
+      .body(StringBody("{\n  \"data\": {},\n  \"event\": {\n    \"id\": \"CreateClaim\",\n    \"summary\": \"\",\n    \"description\": \"\"\n  },\n  \"event_token\": \"${New_Case_event_token}\",\n  \"ignore_warning\": false,\n  \"draft_id\": null\n}"))
       .check(jsonPath("$.id").saveAs("New_Case_Id")))
-  }
 
   .pause(MinThinkTime seconds, MaxThinkTime seconds)
+//
+//  .exec { session =>
+//    val s1 = new File("src/gatling/resources/CMCData.csv")
+//    val writer = new PrintWriter(new FileOutputStream(new File("/src/gatling/resources/CMCData.csv"), true))
+//    writer.write(session("New_Case_Id").as[String].trim)
+//    writer.write(",")
+//    writer.write(session("New_Case_Id").as[String].trim)
+//    writer.close()
+//    session
+//  }
 
-  val PrintCaseID = exec {
-    session =>
-      println(session("New_Case_Id").as[String])
-      session
-  }
-
-  val CMCSubmitPayment = group("CMC_SubmitPayment")
-  {
-    exec(http("CMC_040_005_SubmitPaymentPage")
-      .get(BaseURL + "/data/internal/cases/${New_Case_Id}/event-triggers/SubmitPostPayment?ignore-warning=false")
+  val CMCStayCase = exec(http("CMC_040_005_StayClaim")
+      .get("/data/internal/cases/${New_Case_Id}/event-triggers/StayClaim?ignore-warning=false")
       .headers(headers_11)
       .check(jsonPath("$.event_token").saveAs("existing_case_event_token")))
 
-      .pause(MinThinkTime seconds, MaxThinkTime seconds)
+    .pause(MinThinkTime seconds, MaxThinkTime seconds)
 
-    /*.exec(http("CMC_040_010_SubmitPayment")
-      .post(BaseURL + "/data/caseworkers/:uid/jurisdictions/CMC/case-types/MoneyClaimCase/cases/${New_Case_Id}/events")
-      .headers(CommonHeader)
-      .body(StringBody("{\n  \"data\": {\n    \"interestDateType\": \"CUSTOM\",\n    \"interestType\": \"BREAKDOWN\",\n    \"subjectType\": \"CLAIMANT\",\n    \"amountType\": \"NOT_KNOWN\",\n    \"interestEndDateType\": \"SUBMISSION\",\n    \"evidence\": [],\n    \"defendants\": [],\n    \"claimants\": [],\n    \"timeline\": [],\n    \"feeCode\": null,\n    \"interestRate\": null,\n    \"interestReason\": null,\n    \"interestBreakDownAmount\": null,\n    \"interestBreakDownExplanation\": null,\n    \"interestSpecificDailyAmount\": null,\n    \"interestClaimStartDate\": \"2019-08-01\",\n    \"interestStartDateReason\": null,\n    \"housingDisrepairOtherDamages\": null,\n    \"housingDisrepairCostOfRepairDamages\": null,\n    \"personalInjuryGeneralDamages\": null,\n    \"issuedOn\": \"2019-08-30\",\n    \"submittedOn\": \"2019-08-30T12:00:00.000\",\n    \"submitterEmail\": null,\n    \"id\": null,\n    \"features\": null,\n    \"subjectName\": null,\n    \"paymentId\": null,\n    \"paymentAmount\": null,\n    \"paymentReference\": null,\n    \"paymentStatus\": null,\n    \"submitterId\": null,\n    \"paymentDateCreated\": \"2019-08-30\",\n    \"externalId\": \"cmc-id\",\n    \"referenceNumber\": null,\n    \"feeAmountInPennies\": null,\n    \"reason\": null,\n    \"sotSignerName\": null,\n    \"sotSignerRole\": null,\n    \"feeAccountNumber\": null,\n    \"externalReferenceNumber\": null,\n    \"preferredCourt\": null\n  },\n  \"event\": {\n    \"id\": \"SubmitPostPayment\",\n    \"summary\": \"\",\n    \"description\": \"\"\n  },\n  \"event_token\": \"${existing_case_event_token}\",\n  \"ignore_warning\": false\n}")))
-*/
-    .exec(http("CMC_040_010_SubmitPaymentDetails")
-      .post("/data/case-types/MoneyClaimCase/validate?pageId=SubmitPostPayment1")
-      .headers(headers_3)
-      .body(StringBody("{\n  \"data\": {\n    \"interestDateType\": null,\n    \"interestType\": null,\n    \"subjectType\": null,\n    \"amountType\": null,\n    \"interestEndDateType\": null,\n    \"evidence\": [],\n    \"defendants\": [],\n    \"claimants\": [],\n    \"timeline\": [],\n    \"feeCode\": null,\n    \"interestRate\": null,\n    \"interestReason\": null,\n    \"interestBreakDownAmount\": null,\n    \"interestBreakDownExplanation\": null,\n    \"interestSpecificDailyAmount\": null,\n    \"interestClaimStartDate\": null,\n    \"interestStartDateReason\": null,\n    \"housingDisrepairOtherDamages\": null,\n    \"housingDisrepairCostOfRepairDamages\": null,\n    \"personalInjuryGeneralDamages\": null,\n    \"issuedOn\": null,\n    \"submittedOn\": null,\n    \"submitterEmail\": null,\n    \"id\": null,\n    \"features\": null,\n    \"subjectName\": null,\n    \"paymentId\": null,\n    \"paymentAmount\": null,\n    \"paymentReference\": null,\n    \"paymentStatus\": null,\n    \"submitterId\": null,\n    \"paymentDateCreated\": null,\n    \"externalId\": null,\n    \"referenceNumber\": null,\n    \"feeAmountInPennies\": null,\n    \"reason\": null,\n    \"sotSignerName\": null,\n    \"sotSignerRole\": null,\n    \"feeAccountNumber\": null,\n    \"externalReferenceNumber\": null,\n    \"preferredCourt\": null\n  },\n  \"event\": {\n    \"id\": \"SubmitPostPayment\",\n    \"summary\": \"\",\n    \"description\": \"\"\n  },\n  \"event_token\": \"${existing_case_event_token}\",\n  \"ignore_warning\": false,\n  \"event_data\": {\n    \"interestDateType\": null,\n    \"interestType\": null,\n    \"subjectType\": null,\n    \"amountType\": null,\n    \"interestEndDateType\": null,\n    \"evidence\": [],\n    \"defendants\": [],\n    \"claimants\": [],\n    \"timeline\": [],\n    \"feeCode\": null,\n    \"interestRate\": null,\n    \"interestReason\": null,\n    \"interestBreakDownAmount\": null,\n    \"interestBreakDownExplanation\": null,\n    \"interestSpecificDailyAmount\": null,\n    \"interestClaimStartDate\": null,\n    \"interestStartDateReason\": null,\n    \"housingDisrepairOtherDamages\": null,\n    \"housingDisrepairCostOfRepairDamages\": null,\n    \"personalInjuryGeneralDamages\": null,\n    \"issuedOn\": null,\n    \"submittedOn\": null,\n    \"submitterEmail\": null,\n    \"id\": null,\n    \"features\": null,\n    \"subjectName\": null,\n    \"paymentId\": null,\n    \"paymentAmount\": null,\n    \"paymentReference\": null,\n    \"paymentStatus\": null,\n    \"submitterId\": null,\n    \"paymentDateCreated\": null,\n    \"externalId\": null,\n    \"referenceNumber\": null,\n    \"feeAmountInPennies\": null,\n    \"reason\": null,\n    \"sotSignerName\": null,\n    \"sotSignerRole\": null,\n    \"feeAccountNumber\": null,\n    \"externalReferenceNumber\": null,\n    \"preferredCourt\": null\n  },\n  \"case_reference\": \"${New_Case_Id}\"\n}")))
+    .exec(http("CMC_040_005_StayClaimSubmit")
+      .post("/data/cases/${New_Case_Id}/events")
+      .headers(headers_9)
+      .body(StringBody("{\n  \"data\": {},\n  \"event\": {\n    \"id\": \"StayClaim\",\n    \"summary\": \"\",\n    \"description\": \"\"\n  },\n  \"event_token\": \"${existing_case_event_token}\",\n  \"ignore_warning\": false\n}")))
 
-    .exec(http("CMC_040_015_SubmitPayment")
-      .post("/data/caseworkers/:uid/jurisdictions/CMC/case-types/MoneyClaimCase/cases/${New_Case_Id}/events")
-      .headers(CommonHeader)
-      .body(StringBody("{\n  \"data\": {\n    \"interestDateType\": null,\n    \"interestType\": null,\n    \"subjectType\": null,\n    \"amountType\": null,\n    \"interestEndDateType\": null,\n    \"evidence\": [],\n    \"defendants\": [],\n    \"claimants\": [],\n    \"timeline\": [],\n    \"feeCode\": null,\n    \"interestRate\": null,\n    \"interestReason\": null,\n    \"interestBreakDownAmount\": null,\n    \"interestBreakDownExplanation\": null,\n    \"interestSpecificDailyAmount\": null,\n    \"interestClaimStartDate\": null,\n    \"interestStartDateReason\": null,\n    \"housingDisrepairOtherDamages\": null,\n    \"housingDisrepairCostOfRepairDamages\": null,\n    \"personalInjuryGeneralDamages\": null,\n    \"issuedOn\": null,\n    \"submittedOn\": null,\n    \"submitterEmail\": null,\n    \"id\": null,\n    \"features\": null,\n    \"subjectName\": null,\n    \"paymentId\": null,\n    \"paymentAmount\": null,\n    \"paymentReference\": null,\n    \"paymentStatus\": null,\n    \"submitterId\": null,\n    \"paymentDateCreated\": null,\n    \"externalId\": null,\n    \"referenceNumber\": null,\n    \"feeAmountInPennies\": null,\n    \"reason\": null,\n    \"sotSignerName\": null,\n    \"sotSignerRole\": null,\n    \"feeAccountNumber\": null,\n    \"externalReferenceNumber\": null,\n    \"preferredCourt\": null\n  },\n  \"event\": {\n    \"id\": \"SubmitPostPayment\",\n    \"summary\": \"\",\n    \"description\": \"\"\n  },\n  \"event_token\": \"${existing_case_event_token}\",\n  \"ignore_warning\": false\n}")))
-  }
+    .pause(MinThinkTime seconds, MaxThinkTime seconds)
 
-  .pause(MinThinkTime seconds, MaxThinkTime seconds)
+  val CMCWaitingTransfer = exec(http("CMC_050_005_WaitingTransfer")
+    .get("/data/internal/cases/${New_Case_Id}/event-triggers/WaitingTransfer?ignore-warning=false")
+    .headers(headers_11)
+    .check(jsonPath("$.event_token").saveAs("existing_case_event_token")))
 
-  val CMCSearchAndView = group("CMC_View") {
-    exec(http("CMC_050_005_SearchPage")
+    .pause(MinThinkTime seconds, MaxThinkTime seconds)
+
+    .exec(http("CMC_050_005_WaitingTransferSubmit")
+      .post("/data/cases/${New_Case_Id}/events")
+      .headers(headers_9)
+      .body(StringBody("{\n  \"data\": {},\n  \"event\": {\n    \"id\": \"WaitingTransfer\",\n    \"summary\": \"\",\n    \"description\": \"\"\n  },\n  \"event_token\": \"${existing_case_event_token}\",\n  \"ignore_warning\": false\n}")))
+
+    .pause(MinThinkTime seconds, MaxThinkTime seconds)
+
+  val CMCTransfer = exec(http("CMC_060_005_Transfer")
+    .get("/data/internal/cases/${New_Case_Id}/event-triggers/Transfer?ignore-warning=false")
+    .headers(headers_11)
+    .check(jsonPath("$.event_token").saveAs("existing_case_event_token")))
+
+    .pause(MinThinkTime seconds, MaxThinkTime seconds)
+
+    .exec(http("CMC_060_005_TransferSubmit")
+      .post("/data/cases/${New_Case_Id}/events")
+      .headers(headers_9)
+      .body(StringBody("{\n  \"data\": {},\n  \"event\": {\n    \"id\": \"Transfer\",\n    \"summary\": \"\",\n    \"description\": \"\"\n  },\n  \"event_token\": \"${existing_case_event_token}\",\n  \"ignore_warning\": false\n}")))
+
+    .pause(MinThinkTime seconds, MaxThinkTime seconds)
+
+  val CMCAttachScannedDocs = exec(http("CMC_070_005_AttachScannedDocs")
+    .get("/data/internal/cases/${New_Case_Id}/event-triggers/attachScannedDocs?ignore-warning=false")
+    .headers(headers_11)
+    .check(jsonPath("$.event_token").saveAs("existing_case_event_token")))
+
+    .pause(MinThinkTime seconds, MaxThinkTime seconds)
+
+    .exec(http("CMC_070_005_AttachScannedDocsSubmit")
+      .post("/data/cases/${New_Case_Id}/events")
+      .headers(headers_9)
+      .body(StringBody("{\n  \"data\": {},\n  \"event\": {\n    \"id\": \"attachScannedDocs\",\n    \"summary\": \"\",\n    \"description\": \"\"\n  },\n  \"event_token\": \"${existing_case_event_token}\",\n  \"ignore_warning\": false\n}")))
+
+    .pause(MinThinkTime seconds, MaxThinkTime seconds)
+
+  val CMCSupportUpdate = exec(http("CMC_080_005_SupportUpdate")
+    .get("/data/internal/cases/${New_Case_Id}/event-triggers/SupportUpdate?ignore-warning=false")
+    .headers(headers_11)
+    .check(jsonPath("$.event_token").saveAs("existing_case_event_token")))
+
+    .pause(MinThinkTime seconds, MaxThinkTime seconds)
+
+    .exec(http("CMC_080_005_SupportUpdateSubmit")
+      .post("/data/cases/${New_Case_Id}/events")
+      .headers(headers_9)
+      .body(StringBody("{\n  \"data\": {},\n  \"event\": {\n    \"id\": \"SupportUpdate\",\n    \"summary\": \"\",\n    \"description\": \"\"\n  },\n  \"event_token\": \"${existing_case_event_token}\",\n  \"ignore_warning\": false\n}")))
+
+    .pause(MinThinkTime seconds, MaxThinkTime seconds)
+
+  val CMCSearchAndView = exec(http("CMC_090_005_SearchPage")
       .get("/data/internal/case-types/MoneyClaimCase/work-basket-inputs")
       .headers(headers_0))
 
     .pause(MinThinkTime seconds, MaxThinkTime seconds)
 
-    .exec(http("CMC_050_010_SearchForCase")
+    .exec(http("CMC_090_010_SearchForCase")
       .get("/aggregated/caseworkers/:uid/jurisdictions/CMC/case-types/MoneyClaimCase/cases?view=WORKBASKET&page=1&case_reference=${New_Case_Id}")
       .headers(CommonHeader))
 
     .pause(MinThinkTime seconds, MaxThinkTime seconds)
 
-    .exec(http("CMC_050_015_OpenCase")
+    .exec(http("CMC_090_015_OpenCase")
       .get("/data/caseworkers/:uid/jurisdictions/CMC/case-types/MoneyClaimCase/cases/pagination_metadata?case_reference=${New_Case_Id}")
       .headers(CommonHeader))
-  }
 
-  .pause(MinThinkTime seconds, MaxThinkTime seconds)
+    .pause(MinThinkTime seconds, MaxThinkTime seconds)
 }
