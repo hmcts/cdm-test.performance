7fde0a59b2f4bf7236a61a2bdaea0911cbf82674
jmcadam <mcadam88@gmail.com>
2019-11-24 09:16:05 +0000

    unique user data

--- src/gatling/simulations/uk/gov/hmcts/ccd/corecasedata/scenarios/CreateUser.scala
+++ src/gatling/simulations/uk/gov/hmcts/ccd/corecasedata/scenarios/CreateUser.scala
@@ -13,12 +13,12 @@ object CreateUser {
   val feedUserData = csv("CCDUserData.csv").circular
 
   val headers_1 = Map( //ServiceAuthorization token can be called from http://rpe-service-auth-provider-perftest.service.core-compute-perftest.internal/testing-support/lease
-    "ServiceAuthorization" -> "Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJjY2RfZGVmaW5pdGlvbiIsImV4cCI6MTU3MDcyNjgwMn0.ku3UBt-yGuAyDBaicV7rP5DNSssARe342KC0mN1m2j-erar37iZQ47Cfi6UpXBzh-r3hiy7XruEIpNAzaWEPyg",
+    "ServiceAuthorization" -> "*******",
     "Content-Type" -> "application/json",
     "Accept" -> "application/json")
 
   val headers_0 = Map( //Authorization token needs to be provided by idam team
-    "Authorization" -> "AdminApiAuthToken ****** ",
+    "Authorization" -> "AdminApiAuthToken eyJtb25rZXkiOiIxa1RzZjBxZzdBTGc1UmhNLVh4TlF2SnRzYlEuKkFBSlRTUUFDTURJQUFsTkxBQnczVDNWU2QzbFJVRFZaYmt4WlVtcEhibTFqVWt4T0szZENjRzg5QUFKVE1RQUNNREUuKiIsInJhYmJpdCI6InNlc3Npb24tand0PWV5SjBlWEFpT2lKS1YxUWlMQ0pqZEhraU9pSktWMVFpTENKaGJHY2lPaUpJVXpJMU5pSjkuWlhsS01HVllRV2xQYVVwTFZqRlJhVXhEU214aWJVMXBUMmxLUWsxVVNUUlJNRXBFVEZWb1ZFMXFWVEpKYVhkcFdWZDRia2xxYjJsVmJFNUNUVlk0TVVsdU1DNXRSblpaVUhsMlpXdENSbWhSVjBWbmVVcFRabGMzU2psR2VEUlhRVWhGUjFwQlMwOXFSbTR3UzJKcFQzaGlNMTlVVUUxM09HWktSVEJsUTFWSlRtOUlRbUZuVjJobVdGRkZkMjFIWnpKT09ESjFibkU0U1ZRMFpFWnFaRE16WWt3d00zSTJNbDlwY2pCSGVrSnhVa0V5U0cwM1pVOXRXWGwzVTJacVVVZEhia1phUTJaVVREWm5UV0ZOTUZCbU5IYzJVQzB4YmxwME5FdGlkMk15T0ZoZlJHZEpZMVYyZDBKdVJ6SklhekkwYTNwRVZtRmpPRVkyWkdaa05GTkhVM0pCTXprNWQzbFRiM294VW5WTk1sOWpOVTV1WHpoSGRFTlhWR1J3Y1cxcGFVbEJOVnBqZWxOQ1VHaFdNWE4yZVU5dWVVOTRZVzlIUWsweFUySlVSVGhpYjBKWFFXOTJMVUpOVlc1alExaEhSbmRzUkdnd2VWODVOMEpEWm5vNFMxOXFYM2MxUjJsQk9IWlNTME01ZDFKT05WQnlWVUZxUjNWWWEwc3RUMWRmYVhSclJHeEtaSGQwZERsSGEyMW9ZMGRuVWs1TU9WRXVNMk5XT0RoblFqWjNVVnBsY2w4eFUyMUxTSHBtUVM1dk4zTmZVelpMZURJMVFpMTZSWFpDYW1Gd1ZqQlJlRUpKVWtFemNWVmhRV1l3VGw5VE1raDZka1pzUTI5c1IzcFpkV042YTBKNWFUaEpkeTF5YnpWNmVHVktla1l4ZUVocGRGTnlRbGhCZGtsTlEwRkJSMDFaZUhVeWIyRXhSMDB0ZGpSSkxXWnlkMjVVYTJWQlpuZzVPRXg2UVZORlkxZEhaMVJ4Y0VoTk9WaHpkMGszY0VKcU0xazBNWHBsTFZGSlJUZElXR3hHVUVoSGMzQlRSVWwxWjFCelIyVmhXbU4yV2pCUk9XMDFZazl4Ukd0Uk5tc3RWMWh1U1hnNGVWTjRiME5STjNKQ2EyZzBaa3hDYWs1VVN6TkpiVXhhZEdKM2RVY3pTM2wxYTBGeExXNUZaMnR6U0RKTFdqaFJRa1ZMYVZWelZEWlNSMjEzWVdOUWJrRjRZVXBSTFhaTlQxQTJjSEUxVjNoYVRETmxWRTFOUVU5UGVIUjVNeTFTZEVsNGNGb3lWbVJ6U2pSMGVWaFpMV2xFWTBWblNXVkdVRkZVUlhaQ2MyUk1kbU5PT0hoa09YSjRTV3RIYjNoU1FrSmZZbWhwUVRCU2VEZHlPV3BmY25kTGFsUkJTRU5WY1Raek1rNTNjRlZ3YldodlJIaHRialJhY0VwYWJuZHBTVEJXYldwdk4zRnFjbTlEVjBabFNYaHZhVkEyUTBnd2MzVXRiRWRPVEMwM2N5MXlSVTVCUm1sTGVsODNUa2xQUkVKdFJWTnBkbWgwV1ZoT1JqTlFVREJXVWtWdVRWTllUREZPVm1sSVUxOUdhblp6ZERZNWRFRnBkMTh6ZGxsWlNIRjVjbmxQYXpOcWVHaHdaM0Z0YjJGQlVreFZZbGxHVVhaTlRtWlhVbVkzZG00dFVWRndkRmxxWWtWR1VqZHJRMkYzVEZwalkzaGpNalJ6TUMxdE1HSXlha3RPTjNWSVgwaG9TMGsyVlhaMGJGZGZXbXBFWW5SRlFWa3lhbk5JWW5sSFVHTldjbUpuWjAwelZUVlRPWFpmTmw5c2RXOXdkWFYzVVhCbE5FNDRXV3hTYzFSUVZHODNURTVoT0V4S1VrWmFSSFl4VjJGb1VWVnVlbWxLZVdzMlVuaHdOMUJXVkVOR1NteEdkVEptVTFkNGIyeHVjM0k1TFdrMlNGRlVWVXBIV1daQ1EwNW1lV2RQVFM1aVpXbFhOemhsYkV0bU0xUllTVGxKYjJaUldXNUIua1N2SEhsaS1CMXNFVTBYLXphbjZvS1pNOVJ6eHROV2dQRmw1d2xNOFB5VTsgUGF0aD0vOyBIdHRwT25seSJ9",
     "Content-Type" -> "application/json")
 
 
@@ -78,13 +78,13 @@ object CreateUser {
 
     .exec(http("12_caseworker-divorce-courtadmin")
       .patch("https://idam-api.perftest.platform.hmcts.net/users/${userId}/roles/75a1264c-603c-4109-beff-00eacc9a1ff4")
-      .headers(headers_0))*/
+      .headers(headers_0))
 
     .exec(http("13_payments")
     .patch("https://idam-api.perftest.platform.hmcts.net/users/${userId}/roles/3b8f1382-cd90-4fb0-96e8-82d3eb537e26")
     .headers(headers_0))
 
-    /*.exec(http("1_caseworker-employment")
+    .exec(http("1_caseworker-employment")
       .patch("https://idam-api.perftest.platform.hmcts.net/users/${userId}/roles/c3d17c1b-0426-4e6f-8367-002947097959")
       .headers(headers_0))
 
@@ -111,6 +111,10 @@ object CreateUser {
     .exec(http("7_caseworker-employment-tribunal-glasgow-casesupervisor")
       .patch("https://idam-api.perftest.platform.hmcts.net/users/${userId}/roles/16c3c33c-c880-4a20-9b09-6eabbd6f7ef2")
       .headers(headers_0))*/
+
+    .exec(http("1_caseworker-sscs-superuser")
+    .patch("https://idam-api.perftest.platform.hmcts.net/users/${userId}/roles/6cf46425-53d7-4db9-a6ab-d7c56ed94c08")
+    .headers(headers_0))
 
     .pause(1)
 
--- src/gatling/simulations/uk/gov/hmcts/ccd/corecasedata/simulations/CreateUser.scala
+++ src/gatling/simulations/uk/gov/hmcts/ccd/corecasedata/simulations/CreateUser.scala
@@ -3,7 +3,7 @@ package uk.gov.hmcts.ccd.corecasedata.simulations
 import io.gatling.core.Predef._
 import io.gatling.core.scenario.Simulation
 import io.gatling.http.Predef.Proxy
-import uk.gov.hmcts.ccd.corecasedata.scenarios._
+import uk.gov.hmcts.ccd.corecasedata.scenarios.CreateUser
 import uk.gov.hmcts.ccd.corecasedata.scenarios.utils._
 
 import scala.concurrent.duration._
@@ -17,11 +17,11 @@ class CreateUser extends Simulation  {
     .proxy(Proxy("proxyout.reform.hmcts.net", 8080).httpsPort(8080))
     .doNotTrackHeader("1")
 
-  val CreateIdam = scenario("CCDCreate").repeat(500)
+  val CreateIdam = scenario("CCDCreate").repeat(1000)
   {
     exec(
-      //CreateUser.IdamUser, //These requests add the roles in idam for the user
-      CreateUser.CreateUserProfile //This requests sets the user profile in CCD, which controls what jurisdictions are visible to the user
+      CreateUser.IdamUser //These requests add the roles in idam for the user
+      //CreateUser.CreateUserProfile //This requests sets the user profile in CCD, which controls what jurisdictions are visible to the user
     )
   }
 
