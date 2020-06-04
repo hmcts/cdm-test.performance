24bd052596da7a152f36b977f3ed04e6555bf360
jmcadam <mcadam88@gmail.com>
2020-06-04 09:26:09 +0100

    update case activity value

--- src/gatling/simulations/uk/gov/hmcts/ccd/corecasedata/scenarios/SSCS.scala
+++ src/gatling/simulations/uk/gov/hmcts/ccd/corecasedata/scenarios/SSCS.scala
@@ -15,7 +15,7 @@ object SSCS {
   val feedUserData = csv("SSCSUserData.csv").circular
   val MinThinkTime = Environment.minThinkTime
   val MaxThinkTime = Environment.maxThinkTime
-  val sscsCaseActivityRepeat = 1
+  val sscsCaseActivityRepeat = 5
 
   // val headers_0 = Map(
   //   "Sec-Fetch-Mode" -> "cors",
