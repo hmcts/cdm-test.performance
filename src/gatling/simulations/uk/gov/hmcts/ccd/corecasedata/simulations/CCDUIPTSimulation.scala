c006d7982e8ffddcf21de222f3872f20c3ec0b43
jmcadam <mcadam88@gmail.com>
2020-09-01 12:12:47 +0100

    include CCD UI

--- src/gatling/simulations/uk/gov/hmcts/ccd/corecasedata/simulations/CCDUIPTSimulation.scala
+++ src/gatling/simulations/uk/gov/hmcts/ccd/corecasedata/simulations/CCDUIPTSimulation.scala
@@ -2,6 +2,7 @@ package uk.gov.hmcts.ccd.corecasedata.simulations
 
 import io.gatling.core.Predef._
 import io.gatling.core.scenario.Simulation
+import com.typesafe.config.{Config, ConfigFactory}
 //import io.gatling.http.Predef._ //comment out for VM runs, only required for proxy
 import uk.gov.hmcts.ccd.corecasedata.scenarios._
 import uk.gov.hmcts.ccd.corecasedata.scenarios.utils._
@@ -9,6 +10,7 @@ import scala.concurrent.duration._
 
 class CCDUIPTSimulation extends Simulation  {
 
+  val config: Config = ConfigFactory.load()
   val BaseURL = Environment.baseURL
   val PBiteration = 7 //7
   val SSCSiteration = 14 //14
@@ -17,6 +19,8 @@ class CCDUIPTSimulation extends Simulation  {
   val Fpliteration = 10 //10
   val Ethositeration = 26 //26
   val LFUiteration = 10 //8
+  val csIterationLarge = 200
+  val csIterationSmall = 200
 
   val httpProtocol = Environment.HttpProtocol
     .baseUrl(BaseURL)
@@ -125,13 +129,40 @@ class CCDUIPTSimulation extends Simulation  {
         .exec(WaitforNextIteration.waitforNextIteration)
       }
 
+  val CaseSharingLarge = scenario("CCDCSLarge")
+    .repeat(1) {
+      repeat(1) {
+        exec(casesharing.CDSGetRequestLarge) //this step only required once per user to generate the token for case creation
+        .exec(casesharing.CaseShareLoginLarge) //this step only required once per user to generate the token for case sharing
+      }
+        .repeat(csIterationLarge) {
+            exec(casesharing.CreateCase)
+            .exec(casesharing.CaseSharingPostLarge)
+        }
+    }
+
+  val CaseSharingSmall = scenario("CCDCSSmall")
+    .repeat(1) {
+      repeat(1) {
+        exec(casesharing.CDSGetRequestSmall) //this step only required once per user to generate the token for case creation
+        .exec(casesharing.CaseShareLoginSmall) //this step only required once per user to generate the token for case sharing
+      }
+        .repeat(csIterationSmall) {
+          exec(casesharing.CreateCase)
+          .exec(casesharing.CaseSharingPostSmall)
+        }
+    }
+
   setUp(
     //These 5 scenarios required for CCD regression testing
     CCDProbateScenario.inject(rampUsers(150) during (20 minutes)),
     CCDSSCSScenario.inject(rampUsers(150) during (20 minutes)),
     CCDEthosScenario.inject(rampUsers(400) during (20 minutes)),
     CCDCMCScenario.inject(rampUsers(150) during (20 minutes)),
-    CCDDivScenario.inject(rampUsers(150) during (20 minutes))
+    CCDDivScenario.inject(rampUsers(150) during (20 minutes)),
+    CaseSharingLarge.inject(rampUsers(100) during(20 minutes)),
+    CaseSharingSmall.inject(rampUsers(100) during(20 minutes))
+
 
     //UserProfileSearch.inject(rampUsers(10) during(20 minutes))
 
