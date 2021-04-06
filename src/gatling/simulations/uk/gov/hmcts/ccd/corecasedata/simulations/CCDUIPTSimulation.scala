674edd6936e62f22e691cd2aff6832f26495c519
jmcadam <mcadam88@gmail.com>
2021-04-06 13:05:15 +0100

    Update CCDUIPTSimulation.scala

--- src/gatling/simulations/uk/gov/hmcts/ccd/corecasedata/simulations/CCDUIPTSimulation.scala
+++ src/gatling/simulations/uk/gov/hmcts/ccd/corecasedata/simulations/CCDUIPTSimulation.scala
@@ -233,11 +233,11 @@ class CCDUIPTSimulation extends Simulation  {
   //CCD Regression UI Scenario
   setUp(
     //These 5 scenarios required for CCD regression testing (case activity added 28/01/2021)
-    CCDProbateScenario.inject(rampUsers(150) during (10 minutes)), //150
-    CCDSSCSScenario.inject(rampUsers(150) during (10 minutes)), //150
-    CCDEthosScenario.inject(rampUsers(400) during (10 minutes)), //400
-    CCDCMCScenario.inject(rampUsers(150) during (10 minutes)), //150
-    CCDDivScenario.inject(rampUsers(150) during (10 minutes)), //150
+    CCDProbateScenario.inject(rampUsers(150) during (20 minutes)), //150
+    CCDSSCSScenario.inject(rampUsers(150) during (20 minutes)), //150
+    CCDEthosScenario.inject(rampUsers(400) during (20 minutes)), //400
+    CCDCMCScenario.inject(rampUsers(150) during (20 minutes)), //150
+    CCDDivScenario.inject(rampUsers(150) during (20 minutes)), //150
     CaseActivityScn.inject(rampUsers(100) during (20 minutes)) //100
 
     // CaseActivityScn.inject(rampUsers(1000) during (20 minutes)) //100
@@ -245,9 +245,9 @@ class CCDUIPTSimulation extends Simulation  {
     // XUICaseActivityScn.inject(rampUsers(500) during (20 minutes)), //600
     // CCDCaseActivityScn.inject(rampUsers(1000) during (20 minutes)) //400
 
-    // CCDDivScenario.inject(rampUsers(1) during (1 minutes)), //150
+    // CCDCMCScenario.inject(rampUsers(1) during (1 minutes)), //150
     // CaseActivityScn.inject(rampUsers(1) during (1 minutes)),
   )
     .protocols(httpProtocol)
-    .maxDuration(70 minutes)
+    .maxDuration(80 minutes)
 }
