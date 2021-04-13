bee69b0e93d28f5e2363f19985cec912d9ad5c97
jmcadam <mcadam88@gmail.com>
2021-04-13 15:08:02 +0100

    Update Environment.scala

--- src/gatling/simulations/uk/gov/hmcts/ccd/corecasedata/scenarios/utils/Environment.scala
+++ src/gatling/simulations/uk/gov/hmcts/ccd/corecasedata/scenarios/utils/Environment.scala
@@ -15,8 +15,8 @@ object Environment {
   val xuiBaseURL = "https://manage-case.perftest.platform.hmcts.net"
   val dmStore = "http://dm-store-perftest.service.core-compute-perftest.internal"
 
-  val minThinkTime = 1 //10
-  val maxThinkTime = 3 //30
+  val minThinkTime = 10 //10
+  val maxThinkTime = 30 //30
   val constantthinkTime = 7 //7
   val xuiCaseActivityPause = 30
   val ccdCaseActivityPause = 5
